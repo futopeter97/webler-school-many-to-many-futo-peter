27a0e7a7acec000218109a28193d969163530085
Peter Futo <futopeter97@gmail.com>
2024-03-29 11:39:39 +0100

    Remove unused imports

--- src/main/java/hu/webler/weblerschoolmanytomany/model/SchoolCreateModel.java
+++ src/main/java/hu/webler/weblerschoolmanytomany/model/SchoolCreateModel.java
@@ -1,12 +1,10 @@
 package hu.webler.weblerschoolmanytomany.model;
 
-import hu.webler.weblerschoolmanytomany.entity.Course;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
 
 @Getter
 @Setter
